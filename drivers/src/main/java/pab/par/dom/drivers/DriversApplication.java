49ffaad5c5942acb3878641ff788ba7dd304d362
pabloparra <pabloparradominguez@gmail.com>
2018-07-03 09:02:26 +0200

    initial commit

---
+++ drivers/src/main/java/pab/par/dom/drivers/DriversApplication.java
@@ -0,0 +1,12 @@
+package pab.par.dom.drivers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriversApplication.class, args);
+	}
+}
---
+++ drivers/src/test/java/pab/par/dom/drivers/DriversApplicationTests.java
@@ -0,0 +1,16 @@
+package pab.par.dom.drivers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DriversApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
